ba3ff43792644cb7049d4274d8c0213e6f980e3b
s encheva <encheva.s1@gmail.com>
2024-03-08 13:30:55 +0200

    first commit

---
+++ 8MartIzpitvane/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _8MartIzpitvane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi chislo");
+            string intput = Console.ReadLine();
+            int[] arr = intput.Split(',').Select(int.Parse).ToArray();
+
+            Console.WriteLine(string.Join("," , intput));
+        }
+        static double CulculateAverage(int[] arr)
+        {
+            return arr.Sum() / arr.Length;
+        }
+        static void PrintArray(int[] arr)
+        {
+            Console.WriteLine(string.Join("," , arr));
+        }
+        static int CountZero(int[] arr)
+        {
+            return arr.Length;
+            Console.WriteLine("Broi na nulite v masiva e");
+        }
+      
+
+    }
+}
